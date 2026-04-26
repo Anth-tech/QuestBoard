b92ec05b3a7626d89cf3c1436e4cb4a5acdf3b60
magicninja103 <134741941+magicninja103@users.noreply.github.com>
2026-04-26 01:29:47 -0500

    Update page.js

--- src/app/discussions/page.js
+++ src/app/discussions/page.js
@@ -24,11 +24,11 @@ export default function DiscussionPage() {
 
   return (
     <div style={styles.container}>
-      
+
       {/* Header */}
       <div style={styles.header}>
         <h1>Discussion Boards</h1>
-
+        <h2>{selectedProject ? selectedProject.name : "None selected"}</h2>
         <button style={styles.button}>
           + Create Post
         </button>
